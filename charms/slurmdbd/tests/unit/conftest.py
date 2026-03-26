9b8fd9fd3bf8a5c2428eca2326512efb45901658
Jason C. Nucciarone <nuccitheboss@ubuntu.com>
2026-03-26 17:47:48 -0400

    test(slurmdbd): add unit tests for event handlers
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>

--- charms/slurmdbd/tests/unit/conftest.py
+++ charms/slurmdbd/tests/unit/conftest.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Canonical Ltd.
+# Copyright 2025-2026 Canonical Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,10 +16,46 @@
 
 import pytest
 from charm import SlurmdbdCharm
+from constants import PEER_INTEGRATION_NAME
 from ops import testing
+from pyfakefs.fake_filesystem import FakeFilesystem
+from pytest_mock import MockerFixture
 
 
 @pytest.fixture(scope="function")
-def mock_charm() -> testing.Context[SlurmdbdCharm]:
-    """Mock `SlurmctldCharm`."""
+def peer_integration() -> testing.PeerRelation:
+    """`slurmdbd` peer integration fixture."""
+    return testing.PeerRelation(
+        endpoint=PEER_INTEGRATION_NAME,
+        interface="slurmdbd-peer",
+    )
+
+
+@pytest.fixture(scope="function")
+def mock_ctx() -> testing.Context[SlurmdbdCharm]:
+    """Mock `SlurmdbdCharm` context."""
     return testing.Context(SlurmdbdCharm)
+
+
+@pytest.fixture(scope="function")
+def mock_charm(
+    mock_ctx, mocker: MockerFixture, fs: FakeFilesystem
+) -> testing.Context[SlurmdbdCharm]:
+    """Mock `SlurmdbdCharm` context with fake filesystem.
+
+    Warnings:
+        - The mock charm context must come before the fake filesystem fixture,
+          otherwise `ops.testing.Context` will fail to locate the `slurmdbd` charm's
+          charmcraft.yaml file.
+    """
+    fs.create_dir("/etc/slurm")
+    fs.create_file("/etc/default/slurmdbd", create_missing_dirs=True)
+    mocker.patch("shutil.chown")  # User/group `slurm` doesn't exist on host.
+    mocker.patch("subprocess.run")
+    return mock_ctx
+
+
+@pytest.fixture(scope="function", params=(True, False), ids=("success", "failure"))
+def succeed(request: pytest.FixtureRequest) -> bool:
+    """Parameterize a test to succeed and fail."""
+    return request.param
