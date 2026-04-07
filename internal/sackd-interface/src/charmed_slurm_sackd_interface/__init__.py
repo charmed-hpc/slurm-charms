0d8a8ef2e451e38626816f26cbf6d93170a9477b
Jason C. Nucciarone <nuccitheboss@ubuntu.com>
2026-04-07 16:59:20 -0400

    feat(sackd-interface): add `sackd` interface to repository
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>

---
+++ internal/sackd-interface/src/charmed_slurm_sackd_interface/__init__.py
@@ -0,0 +1,94 @@
+# Copyright 2025-2026 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Integration interface implementation for the `sackd` interface."""
+
+__all__ = [
+    "SackdConnectedEvent",
+    "SackdProvider",
+    "SackdRequirer",
+    "SlurmctldDisconnectedEvent",
+    "SlurmctldReadyEvent",
+    "controller_ready",
+]
+
+
+import ops
+from charmed_hpc_libs.ops import leader
+from charmed_slurm_slurmctld_interface import (
+    SlurmctldDisconnectedEvent,
+    SlurmctldProvider,
+    SlurmctldReadyEvent,
+    SlurmctldRequirer,
+    controller_ready,
+)
+
+_REQUIRED_APP_DATA = {
+    "auth_key_id": lambda value: value != '""',
+    "controllers": lambda value: value != "[]",
+}
+
+
+def _sackd_app_data_validator(data: ops.RelationDataContent) -> bool:
+    """Validate data sent by `slurmctld` stored in the `sackd` integration."""
+    return all(validate(data[k]) for k, validate in _REQUIRED_APP_DATA.items())
+
+
+class SackdConnectedEvent(ops.RelationEvent):
+    """Event emitted when a new `sackd` application is connected to `slurmctld`."""
+
+
+class _SackdRequirerEvents(ops.ObjectEvents):
+    """`sackd` requirer events."""
+
+    sackd_connected = ops.EventSource(SackdConnectedEvent)
+
+
+class SackdProvider(SlurmctldRequirer):
+    """Integration interface implementation for `sackd` service providers.
+
+    This interface should be used on `sackd` units to retrieve controller data
+    from the `slurmctld` application leader.
+    """
+
+    def __init__(self, charm: ops.CharmBase, /, integration_name: str) -> None:
+        super().__init__(
+            charm,
+            integration_name,
+            required_app_data=set(_REQUIRED_APP_DATA),
+            app_data_validator=_sackd_app_data_validator,
+        )
+
+
+class SackdRequirer(SlurmctldProvider):
+    """Integration interface implementation for `sackd` service requirers.
+
+    This interface should be used on the `slurmctld` application leader to provide
+    Slurm controller data to `sackd` units.
+    """
+
+    on = _SackdRequirerEvents()  # type: ignore
+
+    def __init__(self, charm: ops.CharmBase, /, integration_name: str) -> None:
+        super().__init__(charm, integration_name)
+
+        self.framework.observe(
+            self.charm.on[self._integration_name].relation_created,
+            self._on_relation_created,
+        )
+
+    @leader
+    def _on_relation_created(self, event: ops.RelationCreatedEvent) -> None:
+        """Handle when a new `sackd` application is connected to `slurmctld`."""
+        self.on.sackd_connected.emit(event.relation)
---
+++ internal/sackd-interface/tests/unit/test_sackd.py
@@ -0,0 +1,209 @@
+# Copyright 2025-2026 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit tests for the `sackd` integration interface implementation."""
+
+import json
+from collections import defaultdict
+
+import ops
+import pytest
+from charmed_hpc_libs.ops.conditions import refresh, wait_unless
+from charmed_slurm_sackd_interface import (
+    SackdConnectedEvent,
+    SackdProvider,
+    SackdRequirer,
+    SlurmctldDisconnectedEvent,
+    SlurmctldReadyEvent,
+    controller_ready,
+)
+from charmed_slurm_slurmctld_interface import ControllerData
+from ops import testing
+
+SACKD_INTEGRATION_NAME = "sackd"
+EXAMPLE_AUTH_KEY = "xyz123=="
+EXAMPLE_CONTROLLERS = ["127.0.0.1", "127.0.1.1"]
+
+
+class MockSackdProviderCharm(ops.CharmBase):
+    """Mock `sackd` provider charm."""
+
+    def __init__(self, framework: ops.Framework):
+        super().__init__(framework)
+
+        self.slurmctld = SackdProvider(self, SACKD_INTEGRATION_NAME)
+
+        framework.observe(
+            self.slurmctld.on.slurmctld_ready,
+            self._on_slurmctld_ready,
+        )
+
+    @refresh(hook=None)
+    @wait_unless(controller_ready)
+    def _on_slurmctld_ready(self, event: SlurmctldReadyEvent) -> None:
+        data = self.slurmctld.get_controller_data(integration_id=event.relation.id)
+        # Assume `remote_app_data` contains `auth_key` and `controllers` list.
+        assert data.auth_key == EXAMPLE_AUTH_KEY
+        assert data.controllers == EXAMPLE_CONTROLLERS
+
+    def _on_slurmctld_disconnected(self, event: SlurmctldDisconnectedEvent) -> None: ...
+
+
+class MockSackdRequirerCharm(ops.CharmBase):
+    """Mock `sackd` requirer charm."""
+
+    def __init__(self, framework: ops.Framework):
+        super().__init__(framework)
+
+        self.sackd = SackdRequirer(self, SACKD_INTEGRATION_NAME)
+
+        framework.observe(
+            self.sackd.on.sackd_connected,
+            self._on_sackd_connected,
+        )
+
+    def _on_sackd_connected(self, event: SackdConnectedEvent) -> None:
+        self.sackd.set_controller_data(
+            ControllerData(
+                auth_key=EXAMPLE_AUTH_KEY,
+                controllers=EXAMPLE_CONTROLLERS,
+            ),
+            integration_id=event.relation.id,
+        )
+
+
+@pytest.fixture(scope="function")
+def provider_ctx() -> testing.Context[MockSackdProviderCharm]:
+    return testing.Context(
+        MockSackdProviderCharm,
+        meta={
+            "name": "sackd-provider",
+            "provides": {SACKD_INTEGRATION_NAME: {"interface": "sackd"}},
+        },
+    )
+
+
+@pytest.fixture(scope="function")
+def requirer_ctx() -> testing.Context[MockSackdRequirerCharm]:
+    return testing.Context(
+        MockSackdRequirerCharm,
+        meta={
+            "name": "sackd-requirer",
+            "requires": {SACKD_INTEGRATION_NAME: {"interface": "sackd"}},
+        },
+    )
+
+
+@pytest.mark.parametrize(
+    "leader",
+    (
+        pytest.param(True, id="leader"),
+        pytest.param(False, id="not leader"),
+    ),
+)
+class TestSackdInterface:
+    """Unit tests for the `sackd` integration interface implementation."""
+
+    # Test provider-side of the `sackd` interface.
+
+    @pytest.mark.parametrize(
+        "ready",
+        (
+            pytest.param(True, id="ready"),
+            pytest.param(False, id="not ready"),
+        ),
+    )
+    def test_provider_on_slurmctld_ready_event(self, provider_ctx, leader, ready) -> None:
+        """Test that the `sackd` provider waits for controller data."""
+        auth_key_secret = testing.Secret(tracked_content={"key": EXAMPLE_AUTH_KEY})
+
+        sackd_integration_id = 1
+        sackd_integration = testing.Relation(
+            endpoint=SACKD_INTEGRATION_NAME,
+            interface="sackd",
+            id=sackd_integration_id,
+            remote_app_name="sackd-requirer",
+            remote_app_data=(
+                {
+                    "auth_key": '"***"',
+                    "auth_key_id": json.dumps(auth_key_secret.id),
+                    "controllers": json.dumps(EXAMPLE_CONTROLLERS),
+                }
+                if ready
+                else {
+                    "auth_key": '"***"',
+                    "auth_key_id": json.dumps(""),
+                    "controllers": json.dumps([]),
+                }
+            ),
+        )
+
+        state = provider_ctx.run(
+            provider_ctx.on.relation_changed(sackd_integration),
+            testing.State(
+                leader=leader,
+                relations={sackd_integration},
+                secrets={auth_key_secret},
+            ),
+        )
+
+        if ready:
+            # Assert that `SlurmctldReadyEvent` was emitted only once.
+            occurred = defaultdict(lambda: 0)
+            for event in provider_ctx.emitted_events:
+                occurred[type(event)] += 1
+
+            assert occurred[SlurmctldReadyEvent] == 1
+
+            # Assert that there are no deferred events.
+            assert len(state.deferred) == 0
+
+            # Assert that the unit is not waiting for controller data.
+            assert state.unit_status != ops.WaitingStatus("Waiting for controller data")
+        else:
+            # Assert that the charm defers events if there's missing integration data.
+            assert len(state.deferred) == 1
+            assert state.unit_status == ops.WaitingStatus("Waiting for controller data")
+
+    # Test requires-side of the `sackd` interface.
+
+    def test_requirer_on_sackd_connected_event(self, requirer_ctx, leader) -> None:
+        """Test that `slurmctld` sets required integration data for `sackd` to function."""
+        sackd_integration_id = 1
+        sackd_integration = testing.Relation(
+            endpoint=SACKD_INTEGRATION_NAME,
+            interface="sackd",
+            id=sackd_integration_id,
+            remote_app_name="sackd-provider",
+        )
+
+        state = requirer_ctx.run(
+            requirer_ctx.on.relation_created(sackd_integration),
+            testing.State(leader=leader, relations={sackd_integration}),
+        )
+
+        integration = state.get_relation(sackd_integration_id)
+        if leader:
+            # Verify that the leader unit has set the required data for `sackd`.
+            assert "auth_key" in integration.local_app_data
+            assert integration.local_app_data["auth_key"] == '"***"'
+
+            assert "auth_key_id" in integration.local_app_data
+            assert integration.local_app_data["auth_key_id"] != '""'
+
+            assert "controllers" in integration.local_app_data
+            assert integration.local_app_data["controllers"] == json.dumps(EXAMPLE_CONTROLLERS)
+        else:
+            # Verify that non-leader units have not set anything in `local_app_data`.
+            assert integration.local_app_data == {}
