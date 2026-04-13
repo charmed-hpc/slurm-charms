429db9d9694c319f08adfa2122d228de8659a3df
dsloanm <dominic.sloanmurphy@canonical.com>
2026-04-13 19:49:03 +0100

    fix(charmed_slurm_slurmctld_interface): replace AUTH_KEY_LABEL docstring with comment
    Docstrings for attributes is not part of standard Python

--- pkg/slurmctld-interface/src/charmed_slurm_slurmctld_interface/__init__.py
+++ pkg/slurmctld-interface/src/charmed_slurm_slurmctld_interface/__init__.py
@@ -40,9 +40,8 @@ from slurmutils import Model, SlurmConfig
 
 _logger = logging.getLogger(__name__)
 
+# Label for the Juju secret storing the Slurm auth key. Used by multiple Slurm services
 AUTH_KEY_LABEL = "auth-key-secret"
-"""Label for the Juju secret storing the Slurm auth key. Used by multiple Slurm services."""
-
 JWT_KEY_TEMPLATE_LABEL = Template("integration-$id-jwt-key-secret")
 
 
