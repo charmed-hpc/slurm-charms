62c767d53d7c8f9659e5a7051db54811a999786b
Jason C. Nucciarone <nuccitheboss@ubuntu.com>
2026-05-06 15:56:00 +0200

    feat(sackd): update tf modules to be compliant with CC008 specification
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>

--- charms/sackd/terraform/providers.tf
+++ charms/sackd/terraform/providers.tf
@@ -1,4 +1,4 @@
-# Copyright 2024-2026 Canonical Ltd.
+# Copyright 026 Canonical Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,15 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-output "application" {
-  description = "The deployed sackd application resource."
-  value       = juju_application.sackd
-}
-
-output "provides" {
-  description = "Map of provides endpoint names."
-  value = {
-    metrics_endpoint = "metrics-endpoint"
-    slurmctld        = "slurmctld"
-  }
-}
+provider "juju" {}
--- charms/sackd/terraform/terraform.tf
+++ charms/sackd/terraform/terraform.tf
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 terraform {
+  required_version = ">= 1.0"
   required_providers {
     juju = {
       source  = "juju/juju"
